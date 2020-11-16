fe2a288a158bfc893664e9ad9d3f7e1b56eccf69
ishiyk <k1_ishiyama@hotmail.com>
2020-11-16 15:12:24 +0900

    Add files via upload

---
+++ Lab13/mqtt-sub.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import paho.mqtt.client as mqtt
+import time
+import redis
+
+#broker_address = "127.0.0.1"
+broker_address = "test.mosquitto.org"
+RedisHost = "127.0.0.1"
+Topic = "p4p"
+
+r = redis.Redis(host=RedisHost, port='6379')
+
+def on_message(client, userdata, message):
+    m = str(message.payload.decode("utf-8"))
+    print("message received " + m)
+    r.set('RPIvalue',m)
+#    print("message topic=",message.topic)
+#    print("message qos=",message.qos)
+#    print("message retain flag=",message.retain)
+
+
+print("creating new instance")
+#client = mqtt.Client("sub1") #create new instance
+client = mqtt.Client() #create new instance
+client.on_message=on_message #attach function to callback
+print("connecting to broker")
+client.connect(broker_address) #connect to broker
+
+client.loop_start() #start the loop
+
+while True:
+#    print("Topic ", Topic)
+    client.subscribe(Topic)
+    time.sleep(2) # wait
